4b4db973f1703da393854b3f45f64d860a0eca8d
pradeep negi <pradeep.negi@grootsoftwares.com>
2024-12-20 18:20:31 +0530

    brand functionality

---
+++ Hot4/Hot4.ViewModel/ApiModels/BrandModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Hot4.ViewModel.ApiModels
+{
+    public class BrandModel
+    {
+        public byte BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandSuffix { get; set; }
+        public int? WalletTypeId { get; set; }
+        public byte NetworkId { get; set; }
+        public string Network { get; set; }
+        public string Prefix { get; set; }
+    }
+}
