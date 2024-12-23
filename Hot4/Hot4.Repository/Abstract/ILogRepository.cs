7433acc41647efc274a55e0b4f2169c06e253596
pradeep negi <pradeep.negi@grootsoftwares.com>
2024-12-23 15:37:21 +0530

    log repo

---
+++ Hot4/Hot4.Repository/Abstract/ILogRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hot4.DataModel.Models;
+using Hot4.ViewModel.ApiModels;
+
+namespace Hot4.Repository.Abstract
+{
+    public interface ILogRepository
+    {
+        public Task AddLog(Log log);
+        public Task<List<LogModel>> GetAll(int pageNumber, int pageSize);
+    }
+}
---
+++ Hot4/Hot4.ViewModel/ApiModels/LogModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Hot4.ViewModel.ApiModels
+{
+    public class LogModel
+    {
+        public long LogId { get; set; }
+
+        public required DateTime LogDate { get; set; }
+
+        public required string LogModule { get; set; }
+
+        public required string LogObject { get; set; }
+
+        public required string LogMethod { get; set; }
+
+        public required string LogDescription { get; set; }
+
+        public string? Idtype { get; set; }
+
+        public long? Idnumber { get; set; }
+    }
+}
