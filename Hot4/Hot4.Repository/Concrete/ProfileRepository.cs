4c3ff352718685a872278b072e2847eae179d4d2
pradeep negi <pradeep.negi@grootsoftwares.com>
2024-12-30 12:16:02 +0530

    profile work

--- Hot4/Hot4.Repository/Concrete/ProfileRepository.cs
+++ Hot4/Hot4.Repository/Concrete/ProfileRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Hot4.DataModel.Data;
 using Hot4.DataModel.Models;
 using Hot4.Repository.Abstract;
+using Hot4.ViewModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hot4.Repository.Concrete
@@ -8,14 +9,48 @@ namespace Hot4.Repository.Concrete
     public class ProfileRepository : RepositoryBase<Profile>, IProfileRepository
     {
         public ProfileRepository(HotDbContext context) : base(context) { }
-        public async Task<Profile?> GetProfile(int profileId)
+
+        public async Task AddProfile(Profile profile)
         {
-            return await GetById(profileId);
+            await Create(profile);
+            await SaveChanges();
         }
 
-        public async Task<List<Profile>> ListProfile()
+        public async Task DeleteProfile(Profile profile)
         {
-            return await GetAll().ToListAsync();
+            await Delete(profile);
+            await SaveChanges();
+        }
+
+        public async Task<ProfileModel?> GetProfile(int profileId)
+        {
+            var result = await GetById(profileId);
+            if (result != null)
+            {
+                return new ProfileModel
+                {
+                    ProfileId = result.ProfileId,
+                    ProfileName = result.ProfileName,
+                };
+            }
+            return null;
+        }
+
+        public async Task<List<ProfileModel>> ListProfile()
+        {
+            return await GetAll()
+                .Select(d => new ProfileModel
+                {
+                    ProfileId = d.ProfileId,
+                    ProfileName = d.ProfileName,
+                })
+                .ToListAsync();
+        }
+
+        public async Task UpdateProfile(Profile profile)
+        {
+            await Update(profile);
+            await SaveChanges();
         }
     }
 }
---
+++ Hot4/Hot4.ViewModel/ProfileDiscountModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hot4.ViewModel
+{
+    public class ProfileDiscountModel
+    {
+        public int ProfileDiscountId { get; set; }
+        public int ProfileId { get; set; }
+        public byte BrandId { get; set; }
+        public int NetworkId { get; set; }
+        public string Network { get; set; }
+        public string NetworkPrefix { get; set; }
+        public string BrandName { get; set; }
+        public string BrandSuffix { get; set; }
+        [Column(TypeName = "money")]
+        public decimal Discount { get; set; }
+    }
+}
