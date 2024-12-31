1bd482ab7b6a1acc0e3b1445e25f5158e2b5dcbb
pradeep negi <pradeep.negi@grootsoftwares.com>
2024-12-31 14:02:12 +0530

    smpp functionality

--- Hot4/Hot4.Repository/Concrete/SMPPRepository.cs
+++ Hot4/Hot4.Repository/Concrete/SMPPRepository.cs
@@ -2,41 +2,54 @@
 using Hot4.DataModel.Data;
 using Hot4.DataModel.Models;
 using Hot4.Repository.Abstract;
+using Hot4.ViewModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hot4.Repository.Concrete
 {
     public class SMPPRepository : RepositoryBase<Smpp>, ISMPPRepository
     {
         public SMPPRepository(HotDbContext context) : base(context) { }
-        //public async Task<List<SMPPModel>> ListSMPP()
-        //{
-        //    return await GetAll().Select(d => new SMPPModel
-        //    {
 
-        //        AddressRange = d.AddressRange,
-        //        AllowSend = d.AllowSend,
-        //        AllowReceive = d.AllowReceive,
-        //        DestinationAddressNpi = d.DestinationAddressNpi,
-        //        DestinationAddressTon = d.DestinationAddressTon,
-        //        SourceAddress = d.SourceAddress,
-        //        SourceAddressTon = d.SourceAddressTon,
-        //        EconetPrefix = d.EconetPrefix,
-        //        SourceAddressNpi = d.SourceAddressNpi,
-        //        InterfaceVersion = d.InterfaceVersion,
-        //        NetOnePrefix = d.NetOnePrefix,
-        //        RemoteHost = d.RemoteHost,
-        //        RemotePort = d.RemotePort,
-        //        SmppEnabled = d.SmppEnabled,
-        //        SmppID = d.SmppId,
-        //        SmppName = d.SmppName,
-        //        SmppPassword = d.SmppPassword,
-        //        SmppTimeout = d.SmppTimeout,
-        //        SystemID = d.SystemId,
-        //        SystemType = d.SystemType,
-        //        TelecelPrefix = d.TelecelPrefix,
-        //    }).ToListAsync();
+        public async Task AddSMPP(Smpp smpp)
+        {
+            await Create(smpp);
+            await SaveChanges();
+        }
 
+        public async Task<List<SMPPModel>> ListSMPP()
+        {
+            return await GetAll().Select(d => new SMPPModel
+            {
 
-        //}
+                AddressRange = d.AddressRange,
+                AllowSend = d.AllowSend,
+                AllowReceive = d.AllowReceive,
+                DestinationAddressNpi = d.DestinationAddressNpi,
+                DestinationAddressTon = d.DestinationAddressTon,
+                SourceAddress = d.SourceAddress,
+                SourceAddressTon = d.SourceAddressTon,
+                EconetPrefix = d.EconetPrefix,
+                SourceAddressNpi = d.SourceAddressNpi,
+                InterfaceVersion = d.InterfaceVersion,
+                NetOnePrefix = d.NetOnePrefix,
+                RemoteHost = d.RemoteHost,
+                RemotePort = d.RemotePort,
+                SmppEnabled = d.SmppEnabled,
+                SmppId = d.SmppId,
+                SmppName = d.SmppName,
+                SmppPassword = d.SmppPassword,
+                SmppTimeout = d.SmppTimeout,
+                SystemId = d.SystemId,
+                SystemType = d.SystemType,
+                TelecelPrefix = d.TelecelPrefix,
+            }).ToListAsync();
+        }
+
+        public async Task UpdateSMPP(Smpp smpp)
+        {
+            await Update(smpp);
+            await SaveChanges();
+        }
     }
 }
