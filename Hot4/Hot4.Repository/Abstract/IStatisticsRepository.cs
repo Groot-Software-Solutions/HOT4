759b47f9e47d5493787107a28a4eaf94381145aa
pradeep negi <pradeep.negi@grootsoftwares.com>
2025-01-01 21:02:00 +0530

    statitis functionality

---
+++ Hot4/Hot4.Repository/Abstract/IStatisticsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hot4.ViewModel;
+
+namespace Hot4.Repository.Abstract
+{
+    public interface IStatisticsRepository
+    {
+        Task<List<StatisticsTrafficModel>> GetRechargeTrafficStatistics();
+        Task<List<StatisticsTrafficModel>> GetRechargeTrafficDayStatistics();
+        Task<List<StatisticsTrafficModel>> GetSMSTrafficStatistics();
+    }
+}
---
+++ Hot4/Hot4.ViewModel/StatisticsTrafficModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hot4.ViewModel
+{
+    public class StatisticsTrafficModel
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
