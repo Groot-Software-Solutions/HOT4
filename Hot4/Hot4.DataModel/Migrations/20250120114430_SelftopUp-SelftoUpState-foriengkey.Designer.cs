85a3d30c332ed241ef9534280cd2783dae6a6627
pradeep negi <pradeep.negi@grootsoftwares.com>
2025-01-20 17:18:54 +0530

    add foreign key in selftopup model

--- Hot4/Hot4.DataModel/Migrations/20250120114430_SelftopUp-SelftoUpState-foriengkey.Designer.cs
+++ Hot4/Hot4.DataModel/Migrations/20250120114430_SelftopUp-SelftoUpState-foriengkey.Designer.cs
@@ -4,6 +4,7 @@ using Hot4.DataModel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hot4.DataModel.Migrations
 {
     [DbContext(typeof(HotDbContext))]
-    partial class HotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250120114430_SelftopUp-SelftoUpState-foriengkey")]
+    partial class SelftopUpSelftoUpStateforiengkey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
