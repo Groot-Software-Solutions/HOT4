1900dabb43e96c83683ee552fd773b7cb820853f
07Sarbjeet <sarbjeet.singh@grootsoftwares.com>
2025-01-30 16:58:08 +0530

    worked in zesa and tel one service and testing in services
    worked in zesa and tel one service and testing in services

---
+++ Hot4/Hot4.Service/Abstract/IZesaService.cs
@@ -0,0 +1,16 @@
+﻿using Hot4.DataModel.Models;
+using Hot4.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZesaAPI;
+
+namespace Hot4.Service.Abstract
+{
+    public interface IZesaService
+    {
+        Task<ServiceRechargeResponse> CompleteZesaAsync(RechargeModel recharge, PurchaseToken tokenResponse , long smsId);
+    }
+}
